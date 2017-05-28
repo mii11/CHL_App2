6ce68ff65ef0a0ae3207b151e3a7731ccaf69f23
mii11 <batida999@gmail.com>
2017-05-29 00:26:12 +0900

    Added assets

---
+++ Assets/Scripts/CoreController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreController : MonoBehaviour {
+	public GameObject enemyObj;
+	EnemyController enemyController;
+
+
+	void Start(){
+		//EnemyControllerクラスの変数にenemyオブジェクトのコンポーネントを代入する
+		enemyController = enemyObj.GetComponent<EnemyController> ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		print ("CoreのColliderの中");
+
+		if (other.gameObject.tag == "PlayerBullet") {
+			if (gameObject.tag == "Core") {
+				//				print ("Core にあたった");
+				enemyController.curHp = 0;
+				enemyController.Die();
+			}
+		}
+
+	}
+}
